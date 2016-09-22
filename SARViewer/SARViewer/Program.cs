f60e5ff3bd100949b7a19020a360eb8599a099d3
iFialAtFail <mmanle01@gmail.com>
2016-09-22 14:16:42 -0400

    Arbitrary File change to demonstrate GIT
    Just added a few lines of code to main to demonstrate the use of git
    commits and checking out old commits.

--- SARViewer/SARViewer/Program.cs
+++ SARViewer/SARViewer/Program.cs
@@ -10,6 +10,10 @@ namespace SARViewer
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Hello My Friend my friend!");
+            }
         }
     }
 }
