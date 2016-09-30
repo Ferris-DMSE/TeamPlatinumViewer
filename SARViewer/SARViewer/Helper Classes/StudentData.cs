d37ded9f67970dd63d6ff74d23b34aeb9b01a9db
Michael Manley <mmanle01@gmail.com>
2016-09-30 11:01:23 -0400

    Added studentdirectory xml file to project and started working on parsing that data within a studentdata class. Also built the project to make sure the build is good.

--- SARViewer/SARViewer/Helper Classes/StudentData.cs
+++ SARViewer/SARViewer/Helper Classes/StudentData.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace SARViewer.Models
 {
     class StudentData
     {
+        private List<Student> studentDirectory = new List<Student>();
+
+        private List<Student> parseStudents()
+        {
+            List<Student> studentList = new List<Student>();
+
+
+
+
+            return studentList;
+        }
     }
 }
