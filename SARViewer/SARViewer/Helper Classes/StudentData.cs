1fb9625cb3b055accb07d99fb219bbbafc30a749
Michael Manley <mmanle01@gmail.com>
2016-09-30 11:28:14 -0400

    First build of untested StudentData class implementing DataContractSerializer. May have to modify model's to match data contracting

--- SARViewer/SARViewer/Helper Classes/StudentData.cs
+++ SARViewer/SARViewer/Helper Classes/StudentData.cs
@@ -1,25 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.Runtime.Serialization;
 
 namespace SARViewer.Models
 {
+    [Serializable]
     class StudentData
     {
+        private const string FILEPATH = @"C:\Users\USER\Documents\Git\TeamPlatinumViewer\SARViewer\SARViewer\XMLStudentData\studentData.xml";
         private List<Student> studentDirectory = new List<Student>();
+
+        [DataMember]
+        public List<Student> StudentDirectory { get { return studentDirectory; } set { studentDirectory = value; } }
 
         private List<Student> parseStudents()
         {
-            List<Student> studentList = new List<Student>();
+            StudentData sd = new StudentData();
+            if (!File.Exists(FILEPATH))
+            {
+                return null;
+            }
+
+            DataContractSerializer deserializer = new DataContractSerializer(typeof(StudentData));
+
+            using (Stream stream = File.OpenRead(FILEPATH))
+            {
+                 sd = (StudentData)deserializer.ReadObject(stream);
+            }
 
 
-
-
-            return studentList;
+            return sd.StudentDirectory;
         }
     }
 }
