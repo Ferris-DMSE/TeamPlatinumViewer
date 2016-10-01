31594431b1864470e0af8dd4aa52a2e7a872b52d
Josh <jgross0294@gmail.com>
2016-09-30 23:13:48 -0400

    Created properties required for the student class including the list.

--- SARViewer/SARViewer/Models/Student.cs
+++ SARViewer/SARViewer/Models/Student.cs
@@ -8,5 +8,9 @@ namespace SARViewer.Models
 {
     class Student
     {
+        public static int StudentID;
+        public string StudentFirstName = string.Empty;
+        public string StudentLastName = string.Empty;
+        List<string> CourseList = new List<string>();
     }
 }
