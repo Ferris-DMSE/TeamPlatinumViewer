3db472634831e54b3f7fcabc5639b36ed7c82e31
Michael Manley <mmanle01@gmail.com>
2016-10-03 14:17:15 -0400

    Added loop logic to continue searching for SAR's. Some formatting tweeks added as well.

--- SARViewer/SARViewer/Program.cs
+++ SARViewer/SARViewer/Program.cs
@@ -17,37 +17,51 @@ namespace SARViewer
             
             if (data != null)
             {
-                Console.WriteLine("Please enter the name of the student to view their SAR.");
-                string userQuery = Console.ReadLine();
-                userQuery = userQuery.ToUpper();
-                var query = from student in data.StudentDirectory
-                            where student.FirstName == userQuery || student.LastName == userQuery
-                            select student;
-
-                if (query.Count() == 0)
-                    Console.WriteLine("No students by that name.");
-                foreach (Student student in query)
+                char choice;
+                do
                 {
-                    Console.WriteLine("\t" + student.FirstName);
-                    Console.WriteLine("\t" + student.LastName);
-                    Console.WriteLine("\t" + student.ID);
-                    Console.WriteLine("--------------------------------------");
-
-                    foreach (Course course in student.CoursesRegistered)
-                    {
-                        Console.WriteLine(course.ID);
-                        Console.WriteLine(course.Name);
-                        Console.WriteLine(course.Semester);
-                        Console.WriteLine(course.Grade);
-                        Console.WriteLine(course.Credits);
-                        Console.WriteLine();
-                    }
+                    queryAndDisplayLogic(data);
+                    Console.WriteLine("Press 1 to search for another student or press 2 to exit the application.");
+                    choice = Console.ReadKey().KeyChar;
+                    Console.Clear();
                 }
+                while (choice != '2');
+
             }
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             
+        }
+
+        private static void queryAndDisplayLogic(StudentData data)
+        {
+            Console.WriteLine("Please enter the name of the student to view their SAR.");
+            string userQuery = Console.ReadLine();
+            userQuery = userQuery.ToUpper();
+            var query = from student in data.StudentDirectory
+                        where student.FirstName == userQuery || student.LastName == userQuery
+                        select student;
+
+            if (query.Count() == 0)
+                Console.WriteLine("No students by that name.\n");
+            foreach (Student student in query)
+            {
+                Console.WriteLine("\t" + student.FirstName);
+                Console.WriteLine("\t" + student.LastName);
+                Console.WriteLine("\t" + student.ID);
+                Console.WriteLine("--------------------------------------");
+
+                foreach (Course course in student.CoursesRegistered)
+                {
+                    Console.WriteLine(course.ID);
+                    Console.WriteLine(course.Name);
+                    Console.WriteLine(course.Semester);
+                    Console.WriteLine(course.Grade);
+                    Console.WriteLine(course.Credits);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
