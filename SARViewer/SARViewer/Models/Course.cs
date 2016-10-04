58bd5e4cd57f364a2401c39cff1e11c6b56a0fdc
Jake Jones <jakejones506@gmail.com>
2016-10-04 13:58:08 -0400

    Revised course class with auto properties in order to match expected XML structure.

--- SARViewer/SARViewer/Models/Course.cs
+++ SARViewer/SARViewer/Models/Course.cs
@@ -8,84 +8,14 @@ namespace SARViewer.Models
 {
     class Course
     {
-        #region Fields
-        private int courseID;            //All the fields each course requires
-        private string courseNumber;
-        private string courseName;
-        private int credit;
-        private string semester;
-        private int year;
-        private string courseType;
-        private string grade;
-        private bool completed;
-        #endregion
+        public int UniqueID { get; set; }
+        public string CourseNumber { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public int Year { get; set; }
+        public string Semester { get; set; }
+        public string CourseType { get; set; }
+        public string Grade { get; set; }
 
-        #region Properties
-
-        public int CourseID
-        {
-            get { return courseID; }
-            set { courseID = value; }
-        }
-
-        public string CourseNumber
-        {
-            get { return courseNumber; }
-            set { courseNumber = value; }
-        }
-        public string CourseName
-        {
-            get { return courseName; }
-            set { courseName = value; }
-        }
-
-        public int Credit            
-        {
-            get { return credit; }
-            set { credit = value; }
-        }
-        
-        public string Semester
-        {
-            get { return semester; }
-            set { semester = value; }
-        }
-        public int Year
-        {
-            get { return year; }
-            set { year = value; }
-        }
-        public string CourseType
-        {
-            get { return courseType; }
-            set { courseType = value; }
-        }
-        public string Grade
-        {
-            get { return grade; }
-            set { grade = value; }
-        }
-        public bool Completed
-        {
-            get
-            {
-                if (grade != "I" && grade != "W" )
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                } 
-            }
-            set { }
-        }
-        #endregion
-
-
-        public Course() //Empty Constructor 
-        {                                         
-            
-        }
     }
 }
