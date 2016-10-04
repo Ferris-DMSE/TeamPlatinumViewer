a7b6cabacf700c960a752665c7f5d320df3ab63e
Jake Jones <jakejones506@gmail.com>
2016-10-04 13:51:35 -0400

    Created student class

--- SARViewer/SARViewer/Models/Student.cs
+++ SARViewer/SARViewer/Models/Student.cs
@@ -8,5 +8,11 @@ namespace SARViewer.Models
 {
     class Student
     {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public List<Course> CoursesRegistered { get; set; }
+       
     }
 }
