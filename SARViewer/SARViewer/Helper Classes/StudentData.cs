c1a2cf7f49b0a8b4a23e4e83dbec17c55100d1d7
Jake Jones <jakejones506@gmail.com>
2016-10-04 14:23:58 -0400

    Created student Data Class, added serialization reference.

--- SARViewer/SARViewer/Helper Classes/StudentData.cs
+++ SARViewer/SARViewer/Helper Classes/StudentData.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace SARViewer.Models
 {
+    [DataContract(Namespace = "")]
     class StudentData
     {
+        private const string FILEPATH = @"..\..\XMLStudentData\studentData.xml";
+
+        [DataMember]
+        public List<Student> StudentDirectory { get; set; }
+
+        public static StudentData DeserializeFromXML()
+        {
+            StudentData data = new StudentData();
+
+            if (!File.Exists(FILEPATH))
+            {
+                return null;
+            }
+
+            DataContractSerializer Deserializer = new DataContractSerializer(typeof(StudentData));
+
+            using (Stream stream = File.OpenRead(FILEPATH))
+            {
+                data = (StudentData)Deserializer.ReadObject(stream);
+            }
+
+            return data;
+
+        }
+        
+
+
     }
 }
