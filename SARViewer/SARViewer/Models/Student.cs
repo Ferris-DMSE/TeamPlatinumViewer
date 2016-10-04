82edde4737d2119128dd473221c0cfa016555a18
Jake Jones <jakejones506@gmail.com>
2016-10-04 14:32:10 -0400

    Added datamember and order attributes to the student class

--- SARViewer/SARViewer/Models/Student.cs
+++ SARViewer/SARViewer/Models/Student.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace SARViewer.Models
 {
+    [DataContract (Namespace = "")]
     class Student
     {
-        
+        [DataMember(Order = 0)]
         public string FirstName { get; set; }
+        [DataMember(Order = 1)]
         public string LastName { get; set; }
+        [DataMember(Order = 2)]
         public int ID { get; set; }
+        [DataMember(Order = 3)]
         public List<Course> CoursesRegistered { get; set; }
        
     }
