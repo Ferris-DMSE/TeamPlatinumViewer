fe8d16bc78f434d697959e5e9f2cc7a90a713ba7
iFialAtFail <mmanle01@gmail.com>
2016-10-06 14:48:55 -0400

    Basic structure and display fixed. Nearly finished.

--- SARViewer/SARViewer/Program.cs
+++ SARViewer/SARViewer/Program.cs
@@ -29,14 +29,13 @@ namespace SARViewer
                     Console.Clear();                       //Clears console text   
                     if (selection == 'e' || selection == 'E')
                     {
-                        return;
+                        return; // Exit application
                     }
-                    displayQuery(Data);                    //Performs the DisplayQuery Method
+                    displayQuery(Data);//Performs the DisplayQuery Method
 
                     //Pause to keep display
                     Console.WriteLine("Press any key to continue...");
                     Console.ReadKey();
-
 
                 }
 
@@ -51,50 +50,43 @@ namespace SARViewer
             Console.WriteLine("Please enter student's first name"); 
             string studentQuery1 = Console.ReadLine();              //Reads First name input
             studentQuery1 = studentQuery1.ToUpper();                //Puts First name input in all caps so it matches XML file
+
             Console.WriteLine("Please enter student's last name");
             string studentQuery2 = Console.ReadLine();              //Reads Last name input
             studentQuery2 = studentQuery2.ToUpper();                //Puts Last name input in all caps so it matches XML file
 
             var query = from student in data.StudentDirectory      //queries the student directory data                     
-                        where student.FirstName == studentQuery1 || student.LastName == studentQuery2 //Looks for First/Last name values
+                        where student.FirstName == studentQuery1 && student.LastName == studentQuery2 //Looks for First/Last name values
                         select student;  //Selects all students from student directory with that match the query
 
             if (query.Count() == 0)
-                Console.WriteLine("No students found under the name " + studentQuery1 + studentQuery2); //Checks if there was a student under the name provided
-            
+
+                Console.WriteLine("No students found under the name " + studentQuery1 + " " + studentQuery2); //Checks if there was a student under the name provided
+            if (query.Count() == 1)
+            {
+                foreach (Student st in query)
+                {
+                    Console.WriteLine(st.ToString());
+                }
+            }
+
             if (query.Count() > 1)
             {
-                Console.WriteLine("Returned more than one student. Please select the " + 
-                    "number of the student whose Student Academic Record you would like to view.");
+                Console.WriteLine("Returned more than one student. Please enter the students unique ID");
+                int idInput;
+                int.TryParse(Console.ReadLine(), out idInput);
 
-                int k = 0;
-                foreach (Student s in query)
+                var idQuery = from s in data.StudentDirectory
+                              where s.ID == idInput
+                              select s;
+
+                foreach (Student s in idQuery)
                 {
-                    Console.WriteLine("{0}: {1}, {2}", k+1, s.LastName, s.FirstName);
-                    k++;
+                    Console.WriteLine(s.ToString());
                 }
-
-
-            }
-
-            foreach (Student student in query)  //Lists out the matching student's info
-            {
-                Console.WriteLine("\n\nStudent Info:");
-                Console.WriteLine(student.FirstName + " " + student.LastName + "   ID: " + student.ID);
-                Console.WriteLine("");
-                Console.WriteLine("Student's Course List:");
-                Console.WriteLine("-------------------------");
                 
-                foreach (Course course in student.CoursesRegistered)  //Lists out student's courses and details
-                {
-                    Console.WriteLine("\t" + course.Name + ", Course ID:" + course.CourseNumber);                  
-                    Console.WriteLine("\t" + "Semester: " + course.Semester);
-                    Console.WriteLine("\t" + "Grade: " + course.Grade);
-                    Console.WriteLine("\t" + course.Credits + " Credits");
-                    Console.WriteLine("-------------------------");
-                }
-
             }
+            
         }
 
         #endregion
