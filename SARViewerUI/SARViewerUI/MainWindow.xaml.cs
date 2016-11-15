e781c77bb0c6fb55f91ddf390262969af21dc74f
KayK737 <kaykberry@gmail.com>
2016-11-15 13:00:31 -0500

    Update MainWindow.xaml.cs

--- SARViewerUI/SARViewerUI/MainWindow.xaml.cs
+++ SARViewerUI/SARViewerUI/MainWindow.xaml.cs
@@ -39,38 +39,38 @@ namespace SARViewerUI
             }
         }
 
-        private void tbxStudentID_KeyDownHandler(object sender, KeyEventArgs e)
+        private void tbxStudentID_KeyDownHandler(object sender, KeyEventArgs e)//activated when the user presses a button
         {
             
-            if (e.Key == Key.Return)
+            if (e.Key == Key.Return)//display results is prompted when the user hits the enter key
             {
                 DisplayResults(true);
 
             }
         }
 
-        public void btnSearch_Click(object sender, RoutedEventArgs e)
+        public void btnSearch_Click(object sender, RoutedEventArgs e)//prompts when the user clicks the search button
         {
             DisplayResults();
         }
 
         public void btnClear_Click(object sender, RoutedEventArgs e)
         {
-            ClearScreen();
+            ClearScreen();//when the clear button is pressed ClearScreen() is prompted and clears out the screen and search bars
         }
 
-        public void DisplayResults(bool useStudentID = false)
+        public void DisplayResults(bool useStudentID = false)//prompted when the user hits enter or clicks the search button
         {
             List<Student> studentResults = useStudentID ? SearchStudents(tbxStudentID.Text) :SearchStudents(tbxFName.Text, tbxLName.Text, tbxStudentID.Text);
-            String msg = ValidateSearchResults(studentResults);
-            if (!String.IsNullOrEmpty(msg))
+            String msg = ValidateSearchResults(studentResults);//validates the information of the student
+            if (!String.IsNullOrEmpty(msg))//prompts if no information is found
             {
                 MessageBox.Show(msg, "SARViewerUI");
                 ClearScreen();
                 return;
             }
             else
-            {
+            {//when a student is found it gathers the information and puts it into the query
                 Student s = studentResults[0];
                 grdCourses.DataContext = s.CoursesRegistered;
                 tbxStudentID.Text = s.ID.ToString();
@@ -87,14 +87,14 @@ namespace SARViewerUI
                 int GenEdCompleted = completedCourses.Count(c => c.CourseType.ToUpper().Contains("GEN"));
                 int ElectCompleted = completedCourses.Count(c => c.CourseType.ToUpper().Contains("ELECT"));
 
-                tbxCoursesCompleted.Text = coursesCompleted.ToString();
-                tbxCoreCompleted.Text = GetPercentage(CoreCompleted, coursesTaken).ToString();
-                tbxGenEdCompleted.Text = GetPercentage(GenEdCompleted, coursesTaken).ToString();
-                tbxElectiveCompleted.Text = GetPercentage(ElectCompleted, coursesTaken).ToString();
+                tbxCoursesCompleted.Text = coursesCompleted.ToString();//shows courses complete
+                tbxCoreCompleted.Text = GetPercentage(CoreCompleted, coursesTaken).ToString();//gets percetn of core courses completed
+                tbxGenEdCompleted.Text = GetPercentage(GenEdCompleted, coursesTaken).ToString();//gets percent of gen ed courses completed
+                tbxElectiveCompleted.Text = GetPercentage(ElectCompleted, coursesTaken).ToString();//gets percent of elective courses completed
             }
         }
 
-        public void ClearScreen()
+        public void ClearScreen()//class which clears the screen when needed
         {
             tbxFName.Text = String.Empty;
             tbxLName.Text = String.Empty;
@@ -108,12 +108,12 @@ namespace SARViewerUI
             tbxCoursesCompleted.Text = String.Empty;
         }
 
-        public decimal GetPercentage(int part, int whole)
+        public decimal GetPercentage(int part, int whole)//gathers the percent of the courses
         {
             return Decimal.Round((part / Convert.ToDecimal(whole) * 100), 2);
         }
 
-        public string ValidateSearchResults(List<Student> students)
+        public string ValidateSearchResults(List<Student> students)//used to validate student info prompts if student is not found or multiple are found
         {
             if (studentData == null) return "Data file could not be found or loaded.";
             if (students == null || students.Count == 0) return String.Format(SARViewer.Strings.No_Students_Found_0_1, tbxFName.Text, tbxLName.Text);
@@ -121,7 +121,7 @@ namespace SARViewerUI
             return String.Empty;
         }
 
-        public List<Student> SearchStudents(string FName, string LName, string studentID = "")
+        public List<Student> SearchStudents(string FName, string LName, string studentID = "")//puts student information into list to be displayed to user
         {
             if (studentData == null) return null;
 
@@ -154,7 +154,7 @@ namespace SARViewerUI
             return query.ToList();
         }
 
-        public List<Student> SearchStudents(string studentID)
+        public List<Student> SearchStudents(string studentID)//searches for student
         {
             if (studentData == null) return null;
 
