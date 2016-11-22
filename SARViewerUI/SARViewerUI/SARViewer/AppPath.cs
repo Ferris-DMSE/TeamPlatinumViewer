bff6c1c207d9fb97b335e6967c04c7126604e477
iFialAtFail <mmanle01@gmail.com>
2016-11-22 13:33:23 -0500

    Added AppPath class and pdf creator class.

---
+++ SARViewerUI/SARViewerUI/SARViewer/AppPath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SARViewerUI.SARViewer
+{
+    class AppPath
+    {
+        public static string ApplicationPath
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+    }
+}
