4ef819111f8d1c47252cfc2d8135800f81db934c
iFialAtFail <mmanle01@gmail.com>
2016-11-22 13:38:25 -0500

    Added skeleton of PDF generator

--- SARViewerUI/SARViewerUI/SARViewer/PDFCreator.cs
+++ SARViewerUI/SARViewerUI/SARViewer/PDFCreator.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using PdfRpt.Core.Contracts;
+using PdfRpt.FluentInterface;
+using System;
 using System.Collections.Generic;
+using SARViewer.Models;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SARViewerUI.SARViewer
 {
-    class PDFCreator
+    public class PDFCreator
     {
+        public static IPdfReportData CreatePdfReport(List<Student> studentList)
+        {
+            return new PdfReport().DocumentPreferences(doc =>
+            {
+                doc.RunDirection(PdfRunDirection.LeftToRight);
+                doc.Orientation(PageOrientation.Portrait);
+                doc.PageSize(PdfPageSize.A4);
+                doc.DocumentMetadata(new DocumentMetadata
+                {
+                    Author = "Team Platinum",
+                    Application = "PdfRpt",
+                    Keywords = "List Report",
+                    Subject = "Test Report",
+                    Title = "Student Directory"
+                });
+            })
+            .DefaultFonts(fonts =>
+            {
+                fonts.Path(Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\arial.ttf",
+                                  Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\verdana.ttf");
+            })
+            .PagesFooter(footer =>
+            {
+                footer.DefaultFooter(DateTime.Now.ToString("MM/dd/yyyy"));
+            })
+            .PagesHeader(header =>
+            {
+                header.DefaultHeader(defaultHeader =>
+                {
+                    defaultHeader.RunDirection(PdfRunDirection.LeftToRight);
+                    defaultHeader.ImagePath(AppPath.ApplicationPath + "\\Images\\01.png");
+                    defaultHeader.Message("Our new rpt.");
+                });
+            })
+            .MainTableTemplate(template =>
+            {
+                template.BasicTemplate(BasicTemplate.ClassicTemplate);
+            })
+            .MainTablePreferences(table =>
+            {
+                table.ColumnsWidthsType(TableColumnWidthType.Relative);
+                table.NumberOfDataRowsPerPage(5);
+            })
+            .MainTableDataSource(dataSource =>
+            {
+                dataSource.StronglyTypedList(studentList);
+            })
+            .MainTableSummarySettings(summarySettings =>
+            {
+                summarySettings.OverallSummarySettings("Summary");
+                summarySettings.PreviousPageSummarySettings("Previous Page Summary");
+                summarySettings.PageSummarySettings("Page Summary");
+            })
+            .Export(export =>
+            {
+                export.ToExcel();
+                export.ToCsv();
+                export.ToXml();
+            })
+            .Generate(data => data.AsPdfFile(AppPath.ApplicationPath + "\\RptIListSample.pdf"));
+        }
     }
 }
